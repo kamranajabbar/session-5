f0d0ceff49c9f458269da44089ffd3eed21e56b6
Kamran Jabbar <kamranajabbar@gmail.com>
2020-06-21 00:47:15 -0700

    Session 5 Completed

---
+++ src/Child.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import counterContext from './CounterContext';
+
+const Child = () => {
+    let counterValue = useContext(counterContext)
+    //console.log(counterValue)
+
+    return (
+        <div>
+            <h2>This is first child using CounterContext </h2>
+            <h3>Counter value is {counterValue[0]}</h3>
+
+            <button onClick={ ()=>{ counterValue[1](++counterValue[0]) } }>Increament Context</button>
+        </div>
+    );
+}
+
+export default Child;
